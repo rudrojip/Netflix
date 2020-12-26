1412cc8d6601f97edbf9a485df6f33a30ac1268d
rudrojip <35401335+rudrojip@users.noreply.github.com>
2020-12-26 19:12:20 +0530

    Add files via upload

---
+++ NetFlix.Login/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace NetFlix.Login
+{
+    public class Constants
+    {
+        public const string NavigationURL = "https://www.netflix.com/in/";
+        public const string ValidSignInPage = "https://www.netflix.com/in/login";
+        public const string Name_Email = "email";
+        public const string YopMailDomain = "@yopmail.com";
+        public const string InputPath = @"..\..\..\Helpers\rockyou.txt";
+        public const string CheckedEmailPath = @"..\..\..\Helpers\CheckedEmails.txt";
+        public const string OutputFile = @"..\..\..\Helpers\Result.txt";
+    }
+}
